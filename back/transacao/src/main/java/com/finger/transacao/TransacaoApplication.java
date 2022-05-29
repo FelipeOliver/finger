1e73076018a375f2a4a13bd409dc083499b04681
FelipeOliver <bispos_lipe@hotmail.com>
2022-05-29 11:54:39 -0300

    feat: add transacao api initial structure

---
+++ back/transacao/src/main/java/com/finger/transacao/TransacaoApplication.java
@@ -0,0 +1,13 @@
+package com.finger.transacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransacaoApplication.class, args);
+	}
+
+}
---
+++ back/transacao/src/test/java/com/finger/transacao/TransacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finger.transacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
